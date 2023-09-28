8a322f3ca942370b812672aa8e112dd5c51dc80e
Eric86y <eric.werner.mail@gmail.com>
2023-09-28 13:10:05 +0200

    added line kernel calculation, new models, gpu models, cleanup

--- Modules.py
+++ Modules.py
@@ -12,9 +12,8 @@ from Utils import (
     unpatch_image,
     resize_image,
     unpatch_prediction,
-    rotate_page,
+    generate_line_images,
     prepare_ocr_image,
-    preprocess_image_tf
 )
 
 
@@ -33,16 +32,12 @@ class LineDetection:
         self,
         config_file: str,
         binarize_output: bool = False,
-        dilate_kernel: int = 8,
-        dilate_iterations: int = 6,
         execution_providers=None,
     ) -> None:
         self._config_file = config_file
         self._onnx_model_file = None
         self._patch_size = 512
         self._binarize_output = binarize_output
-        self._dilate_kernel = dilate_kernel
-        self._dilate_iterations = dilate_iterations
         self._inference = None
         # add other Execution Providers if applicable, see: https://onnxruntime.ai/docs/execution-providers
         if execution_providers is None:
@@ -54,7 +49,7 @@ class LineDetection:
     def _init(self) -> None:
         _file = open(self._config_file)
         json_content = json.loads(_file.read())
-        self._onnx_model_file = json_content["model"]
+        self._onnx_model_file = json_content["cpu-model"]
         self._patch_size = json_content["patch_size"]
 
         if self._onnx_model_file is not None:
@@ -70,165 +65,6 @@ class LineDetection:
             self.can_run = False
 
         logging.info(f"Line Inference -> Init(): {self.can_run}")
-
-    def get_lines(self, image: np.array, prediction: np.array):
-        line_contours, _ = cv2.findContours(
-            prediction, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
-        )
-
-        x, y, w, h = cv2.boundingRect(prediction)
-
-        if len(line_contours) == 0:
-            return [], None, None
-
-        elif len(line_contours) == 1:
-            bbox_center = (x + w // 2, y + h // 2)
-            peaks = [bbox_center]
-            sorted_contours = {bbox_center: line_contours[0]}
-            line_images = self.get_line_images(image, sorted_contours)
-
-            return line_images, sorted_contours, peaks
-        else:
-            sorted_contours, peaks = self.sort_lines(prediction, line_contours)
-            line_images = self.get_line_images(image, sorted_contours)
-
-            return line_images, sorted_contours, peaks
-
-    def sort_lines(self, line_prediction: np.array, contours: tuple):
-        """
-        A preliminary approach to sort the found contours and sort them by reading lines. The relative distance between the lines is currently taken as roughly constant,
-        wherefore mean // 2 is taken as threshold for line breaks. This might not work in scenarios in which the line distances are less constant.
-
-        Args:
-            - tuple of contours returned by cv2.findContours()
-        Returns:
-            - dictionary of {(bboxcenter_x, bbox_center_y) : [contour]}
-            - peaks returned by find_peaks() marking the line breaks
-        """
-
-        horizontal_projection = np.sum(line_prediction, axis=1)
-        horizontal_projection = horizontal_projection / 255
-        mean = int(np.mean(horizontal_projection))
-        peaks, _ = find_peaks(horizontal_projection, height=mean, width=4)
-
-        # calculate the line distances
-        line_distances = []
-        for idx in range(len(peaks)):
-            if idx < len(peaks) - 1:
-                line_distances.append(
-                    peaks[(len(peaks) - 1) - idx]
-                    - (peaks[(len(peaks) - 1) - (idx + 1)])
-                )
-
-        if len(line_distances) == 0:
-            line_distance = 0
-        else:
-            line_distance = int(
-                np.mean(line_distances)
-            )  # that might not work great if the line distances are varying a lot
-
-        # get the bbox centers of each contour and keep a reference to the contour in contour_dict
-        centers = []
-        contour_dict = {}
-
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            y_center = y + (h // 2)
-            x_center = x + (w // 2)
-            centers.append((x_center, y_center))
-            contour_dict[(x_center, y_center)] = contour
-
-        centers = sorted(centers, key=lambda x: x[1])
-
-        # associate bbox centers with the peaks (i.e. line breaks)
-        cnt_dict = {}
-
-        for center in centers:
-            if center == centers[-1]:
-                cnt_dict[center[1]] = [center]
-                continue
-
-            for peak in peaks:
-                diff = abs(center[1] - peak)
-                if diff <= line_distance // 2:
-                    if peak in cnt_dict.keys():
-                        cnt_dict[peak].append(center)
-                    else:
-                        cnt_dict[peak] = [center]
-
-        # sort bbox centers for x value to get proper reading order
-        for k, v in cnt_dict.items():
-            if len(v) > 1:
-                v = sorted(v)
-                cnt_dict[k] = v
-
-        # build final dictionary with correctly sorted bbox_centers by y and x -> contour
-        sorted_contour_dict = {}
-        for k, v in cnt_dict.items():
-            for l in v:
-                sorted_contour_dict[l] = contour_dict[l]
-
-        return sorted_contour_dict, peaks
-
-    def get_line_images(self, image: np.array, sorted_line_contours: dict):
-        line_images = []
-
-        for _, v in sorted_line_contours.items():
-            image_mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
-            cv2.drawContours(
-                image_mask, [v], contourIdx=0, color=(255, 255, 255), thickness=-1
-            )
-            dilated1 = cv2.dilate(
-                image_mask,
-                kernel=(self._dilate_kernel, self._dilate_kernel),
-                iterations=self._dilate_iterations,
-                borderValue=0,
-                anchor=(-1, 0),
-                borderType=cv2.BORDER_DEFAULT,
-            )
-            dilated2 = cv2.dilate(
-                image_mask,
-                kernel=(self._dilate_kernel, self._dilate_kernel),
-                iterations=self._dilate_iterations,
-                borderValue=0,
-                anchor=(0, 1),
-                borderType=cv2.BORDER_DEFAULT,
-            )
-            combined = cv2.add(dilated1, dilated2)
-            image_masked = cv2.bitwise_and(image, image, mask=combined)
-
-            cropped_img = np.delete(
-                image_masked, np.where(~image_masked.any(axis=1))[0], axis=0
-            )
-            cropped_img = np.delete(
-                cropped_img, np.where(~cropped_img.any(axis=0))[0], axis=1
-            )
-
-            if self._binarize_output:
-                indices = np.where(cropped_img[:, :, 1] == 0)
-                clear = cropped_img.copy()
-                clear[indices[0], indices[1], :] = [255, 255, 255]
-                clear_bw = cv2.cvtColor(clear, cv2.COLOR_BGR2GRAY)
-                _, thresh = cv2.threshold(clear_bw, 170, 255, cv2.THRESH_BINARY)
-
-                line_images.append(thresh)
-            else:
-                line_images.append(cropped_img)
-
-        return line_images
-
-    def generate_line_images(self, image: np.array, prediction: np.array):
-        """
-        Applies some rotation correction to the original image and creates the line images based on the predicted lines.
-        """
-        rotated_img, rotated_prediction, angle = rotate_page(
-            original_image=image, line_mask=prediction
-        )
-        line_images, sorted_contours, peaks = self.get_lines(
-            rotated_img, rotated_prediction
-        )
-
-        return line_images, sorted_contours, peaks
 
     def predict(
         self,
@@ -269,10 +105,11 @@ class LineDetection:
         )
         back_sized_image = back_sized_image.astype(np.uint8)
 
-        line_images, sorted_contours, peaks = self.generate_line_images(
+        # TODO: remove this into a post-processing module
+        line_images, sorted_contours, bbox, peaks = generate_line_images(
             original_image, back_sized_image
         )
-        return back_sized_image, line_images, sorted_contours, peaks
+        return back_sized_image, line_images, sorted_contours, bbox, peaks
 
 
 class OCRInference:
@@ -290,39 +127,38 @@ class OCRInference:
     def _init(self) -> None:
         _file = open(self.config, encoding="utf-8")
         json_content = json.loads(_file.read())
-        self._onnx_model_file = json_content["model"]
+        self._onnx_model_file = json_content["cpu-model"]
         self._input_width = json_content["input_width"]
         self._input_height = json_content["input_height"]
-        self.charset = self.get_model_characters(json_content["charset"])
+        self._input_layer = json_content["input_layer"]
+        self._output_layer = json_content["output_layer"]
+        self._add_channel_dim = json_content["add_channel_dim"]
+        self.charset = json_content["charset"]
         self.ocr_session = ort.InferenceSession(
             self._onnx_model_file, providers=["CPUExecutionProvider"]
         )
 
-    def get_model_characters(self, characters):
-        characters = [c for c in characters]
-        characters.append("[BLK]")
-        characters.insert(0, "[UNK]")
-
-        return characters
-
     def run(self, line_images: list, replace_blank: str = ""):
-        """
-        See comment in Utils.py. So preprocess_image_tf() is used for time being.
-        img_batch = [prepare_ocr_image(x) for x in line_images]
-        """
-        img_batch = [preprocess_image_tf(x) for x in line_images]
+        img_batch = [
+            prepare_ocr_image(
+                x, target_width=self._input_width, target_height=self._input_height
+            )
+            for x in line_images
+        ]
         img_batch = np.array(img_batch)
+
         ort_batch = ort.OrtValue.ortvalue_from_numpy(img_batch)
         ocr_results = self.ocr_session.run_with_ort_values(
-            ["Final"], {"the_input": ort_batch}
+            [self._output_layer], {self._input_layer: ort_batch}
         )
         prediction = ocr_results[0].numpy()
+        prediction = np.transpose(prediction, axes=[1, 0, 2])
+        # print(f"Raw prediction: {prediction.shape}")
 
         predicted_text = []
         for idx in range(prediction.shape[0]):
             pred_line = prediction[idx, :, :]
             text, _ = viterbi_search(pred_line, self.charset)
-            text = text.replace("[BLK]", replace_blank)
 
             if len(text) > 0:
                 predicted_text.append(text)
--- Utils.py
+++ Utils.py
@@ -4,10 +4,19 @@ import math
 import numpy as np
 import tensorflow as tf
 from typing import Tuple
+from datetime import datetime
+from xml.dom import minidom
+from scipy.signal import find_peaks
+import xml.etree.ElementTree as etree
 
 
 def get_file_name(x) -> str:
     return os.path.basename(x).split(".")[0]
+
+
+def read_label(gt_file: str) -> str:
+    f = open(gt_file, "r", encoding="utf-8")
+    return f.readline()
 
 
 def create_dir(dir_name: str) -> None:
@@ -56,13 +65,21 @@ def pad_image(
 
 def resize_to_height(image, target_height: int):
     width_ratio = target_height / image.shape[0]
-    image = cv2.resize(image, (int(image.shape[1] * width_ratio), target_height), interpolation=cv2.INTER_LINEAR)
+    image = cv2.resize(
+        image,
+        (int(image.shape[1] * width_ratio), target_height),
+        interpolation=cv2.INTER_LINEAR,
+    )
     return image
 
 
 def resize_to_width(image, target_width: int):
     width_ratio = target_width / image.shape[1]
-    image = cv2.resize(image, (target_width, int(image.shape[0] * width_ratio)), interpolation=cv2.INTER_LINEAR)
+    image = cv2.resize(
+        image,
+        (target_width, int(image.shape[0] * width_ratio)),
+        interpolation=cv2.INTER_LINEAR,
+    )
     return image
 
 
@@ -119,7 +136,9 @@ def pad_image2(
             f"Info -> equal ratio: {img.shape}, w_ratio: {width_ratio}, h_ratio: {height_ratio}"
         )
 
-    return cv2.resize(out_img, (target_width, target_height), interpolation=cv2.INTER_LINEAR)
+    return cv2.resize(
+        out_img, (target_width, target_height), interpolation=cv2.INTER_LINEAR
+    )
 
 
 def patch_image(
@@ -190,6 +209,8 @@ def rotate_page(
     debug_angles: bool = False,
 ) -> float:
     contours, _ = cv2.findContours(line_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    mask_threshold = (line_mask.shape[0] * line_mask.shape[1]) * 0.001
+    contours = [x for x in contours if cv2.contourArea(x) > mask_threshold]
     angles = [cv2.minAreaRect(x)[2] for x in contours]
 
     # angles = [x for x in angles if abs(x) != 0.0 and x != 90.0]
@@ -207,7 +228,10 @@ def rotate_page(
         mean_angle = -(90 - np.mean(high_angles))
 
     else:
+        # print(f"Defaulting to 0: {angles}")
         mean_angle = 0
+
+    # print(f"Rotation angle: {mean_angle}")
 
     rows, cols = original_image.shape[:2]
     rot_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), mean_angle, 1)
@@ -222,38 +246,360 @@ def rotate_page(
     return rotated_img, rotated_prediction, mean_angle
 
 
+def get_line_images(
+    image: np.array,
+    sorted_line_contours: dict,
+    dilate_kernel: int = 20,
+    binarize: bool = False,
+):
+    line_images = []
+
+    for _, contour in sorted_line_contours.items():
+        image_mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
+        cv2.drawContours(
+            image_mask, [contour], contourIdx=0, color=(255, 255, 255), thickness=-1
+        )
+
+        _, _, _, height = cv2.boundingRect(contour)
+
+        if height < 60:
+            dilate_iterations = dilate_kernel * 1
+        elif height >= 60 and height < 90:
+            dilate_iterations = dilate_kernel * 2
+        else:
+            dilate_iterations = dilate_kernel * 6
+
+        dilated1 = cv2.dilate(
+            image_mask,
+            kernel=(dilate_kernel, dilate_kernel),
+            iterations=dilate_iterations,
+            borderValue=0,
+            anchor=(-1, 0),
+            borderType=cv2.BORDER_DEFAULT,
+        )
+        dilated2 = cv2.dilate(
+            image_mask,
+            kernel=(dilate_kernel, dilate_kernel),
+            iterations=dilate_iterations,
+            borderValue=0,
+            anchor=(0, 1),
+            borderType=cv2.BORDER_DEFAULT,
+        )
+        combined = cv2.add(dilated1, dilated2)
+        image_masked = cv2.bitwise_and(image, image, mask=combined)
+
+        cropped_img = np.delete(
+            image_masked, np.where(~image_masked.any(axis=1))[0], axis=0
+        )
+        cropped_img = np.delete(
+            cropped_img, np.where(~cropped_img.any(axis=0))[0], axis=1
+        )
+
+        if binarize:
+            indices = np.where(cropped_img[:, :, 1] == 0)
+            clear = cropped_img.copy()
+            clear[indices[0], indices[1], :] = [255, 255, 255]
+            clear_bw = cv2.cvtColor(clear, cv2.COLOR_BGR2GRAY)
+            _, thresh = cv2.threshold(clear_bw, 170, 255, cv2.THRESH_BINARY)
+
+            line_images.append(thresh)
+        else:
+            line_images.append(cropped_img)
+
+    return line_images
+
+
+def get_lines(image: np.array, prediction: np.array):
+    line_contours, _ = cv2.findContours(
+        prediction, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+    )
+
+    x, y, w, h = cv2.boundingRect(prediction)
+
+    if len(line_contours) == 0:
+        return [], None, None, None
+
+    elif len(line_contours) == 1:
+        bbox_center = (x + w // 2, y + h // 2)
+        peaks = [bbox_center]
+        sorted_contours = {bbox_center: line_contours[0]}
+        line_images = get_line_images(image, sorted_contours)
+
+        return line_images, sorted_contours, (x, y, w, h), peaks
+    else:
+        sorted_contours, peaks = sort_lines(prediction, line_contours)
+        line_images = get_line_images(image, sorted_contours)
+
+        return line_images, sorted_contours, (x, y, w, h), peaks
+
+
+def sort_lines(line_prediction: np.array, contours: tuple):
+    """
+    A preliminary approach to sort the found contours and sort them by reading lines. The relative distance between the lines is currently taken as roughly constant,
+    wherefore mean // 2 is taken as threshold for line breaks. This might not work in scenarios in which the line distances are less constant.
+
+    Args:
+        - tuple of contours returned by cv2.findContours()
+    Returns:
+        - dictionary of {(bboxcenter_x, bbox_center_y) : [contour]}
+        - peaks returned by find_peaks() marking the line breaks
+    """
+
+    horizontal_projection = np.sum(line_prediction, axis=1)
+    horizontal_projection = horizontal_projection / 255
+    mean = int(np.mean(horizontal_projection))
+    peaks, _ = find_peaks(horizontal_projection, height=mean, width=4)
+
+    # calculate the line distances
+    line_distances = []
+    for idx in range(len(peaks)):
+        if idx < len(peaks) - 1:
+            line_distances.append(
+                peaks[(len(peaks) - 1) - idx] - (peaks[(len(peaks) - 1) - (idx + 1)])
+            )
+
+    if len(line_distances) == 0:
+        line_distance = 0
+    else:
+        line_distance = int(
+            np.mean(line_distances)
+        )  # that might not work great if the line distances are varying a lot
+
+    # get the bbox centers of each contour and keep a reference to the contour in contour_dict
+    centers = []
+    contour_dict = {}
+
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        y_center = y + (h // 2)
+        x_center = x + (w // 2)
+        centers.append((x_center, y_center))
+        contour_dict[(x_center, y_center)] = contour
+
+    centers = sorted(centers, key=lambda x: x[1])
+
+    # associate bbox centers with the peaks (i.e. line breaks)
+    cnt_dict = {}
+
+    for center in centers:
+        if center == centers[-1]:
+            cnt_dict[center[1]] = [center]
+            continue
+
+        for peak in peaks:
+            diff = abs(center[1] - peak)
+            if diff <= line_distance // 2:
+                if peak in cnt_dict.keys():
+                    cnt_dict[peak].append(center)
+                else:
+                    cnt_dict[peak] = [center]
+
+    # sort bbox centers for x value to get proper reading order
+    for k, v in cnt_dict.items():
+        if len(v) > 1:
+            v = sorted(v)
+            cnt_dict[k] = v
+
+    # build final dictionary with correctly sorted bbox_centers by y and x -> contour
+    sorted_contour_dict = {}
+    for k, v in cnt_dict.items():
+        for l in v:
+            sorted_contour_dict[l] = contour_dict[l]
+
+    return sorted_contour_dict, peaks
+
+
+def get_line_images(
+    image: np.array,
+    sorted_line_contours: dict,
+    dilate_kernel: int = 20,
+    binarize: bool = False,
+):
+    line_images = []
+
+    for _, contour in sorted_line_contours.items():
+        image_mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
+        cv2.drawContours(
+            image_mask, [contour], contourIdx=0, color=(255, 255, 255), thickness=-1
+        )
+
+        _, _, _, height = cv2.boundingRect(contour)
+
+        if height < 60:
+            dilate_iterations = dilate_kernel * 1
+        elif height >= 60 and height < 90:
+            dilate_iterations = dilate_kernel * 2
+        else:
+            dilate_iterations = dilate_kernel * 6
+
+        dilated1 = cv2.dilate(
+            image_mask,
+            kernel=(dilate_kernel, dilate_kernel),
+            iterations=dilate_iterations,
+            borderValue=0,
+            anchor=(-1, 0),
+            borderType=cv2.BORDER_DEFAULT,
+        )
+        dilated2 = cv2.dilate(
+            image_mask,
+            kernel=(dilate_kernel, dilate_kernel),
+            iterations=dilate_iterations,
+            borderValue=0,
+            anchor=(0, 1),
+            borderType=cv2.BORDER_DEFAULT,
+        )
+        combined = cv2.add(dilated1, dilated2)
+        image_masked = cv2.bitwise_and(image, image, mask=combined)
+
+        cropped_img = np.delete(
+            image_masked, np.where(~image_masked.any(axis=1))[0], axis=0
+        )
+        cropped_img = np.delete(
+            cropped_img, np.where(~cropped_img.any(axis=0))[0], axis=1
+        )
+
+        if binarize:
+            indices = np.where(cropped_img[:, :, 1] == 0)
+            clear = cropped_img.copy()
+            clear[indices[0], indices[1], :] = [255, 255, 255]
+            clear_bw = cv2.cvtColor(clear, cv2.COLOR_BGR2GRAY)
+            _, thresh = cv2.threshold(clear_bw, 170, 255, cv2.THRESH_BINARY)
+
+            line_images.append(thresh)
+        else:
+            line_images.append(cropped_img)
+
+    return line_images
+
+
+def generate_line_images(image: np.array, prediction: np.array):
+    """
+    Applies some rotation correction to the original image and creates the line images based on the predicted lines.
+    """
+    rotated_img, rotated_prediction, angle = rotate_page(
+        original_image=image, line_mask=prediction
+    )
+    line_images, sorted_contours, bbox, peaks = get_lines(
+        rotated_img, rotated_prediction
+    )
+
+    return line_images, sorted_contours, bbox, peaks
+
+
 def optimize_countour(cnt, e=0.001):
     epsilon = e * cv2.arcLength(cnt, True)
     return cv2.approxPolyDP(cnt, epsilon, True)
 
 
 def prepare_ocr_image(
-    line_image: np.array,
-    target_width: int = 2000,
-    target_height: int = 80,
-    padding: str = "black",
+    line_image: np.array, target_width: int = 2000, target_height: int = 80
 ):
     if len(line_image.shape) > 2:
         line_image = cv2.cvtColor(line_image, cv2.COLOR_BGR2GRAY)
-    line_image = pad_image2(line_image, target_width, target_height, padding=padding)
-    line_image = line_image.astype(np.float32)
-    line_image /= 255.0
-    line_image = np.transpose(line_image, axes=[1, 0])
 
-    return line_image
+    image = pad_image2(
+        line_image,
+        target_width=target_width,
+        target_height=target_height,
+        padding="white",
+    )
+    image = image.reshape((1, target_height, target_width))
+    image = (image / 127.5) - 1.0
+    image = image.astype(np.float32)
+    return image
 
-"""
-Note: There is some yet unclear behaviour that results in worse OCR predictions using preparce_oc_image() and pad_image2() when preprocessing line images, although the results are visually and numerically almost identical. Refer to the Debug_OCR notebook to see the effect.
-""" 
 
-def preprocess_image_tf(image: np.array, target_height: int = 80, target_width: int = 2000):
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=0.8, tileGridSize=(24,24))
-    image = clahe.apply(image)
-    tf_img = tf.expand_dims(image, axis=-1)
-    tf_img = tf.image.resize_with_pad(tf_img, target_height, target_width)
-    tf_img = tf_img / 255.0
-    tf_img = tf.transpose(tf_img, perm=[1, 0, 2])
-    tf_img = tf.squeeze(tf_img)
-    
-    return tf_img.numpy()
+def get_utc_time():
+    t = datetime.now()
+    s = t.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    s = s.split(" ")
+
+    return f"{s[0]}T{s[1]}"
+
+
+def get_text_points(contour):
+    points = ""
+    for box in contour:
+        point = f"{box[0][0]},{box[0][1]} "
+        points += point
+    return points
+
+
+def get_text_line_block(coordinate, index, unicode_text):
+    text_line = etree.Element(
+        "Textline", id="", custom=f"readingOrder {{index:{index};}}"
+    )
+    text_line = etree.Element("TextLine")
+    text_line_coords = coordinate
+
+    text_line.attrib["id"] = f"line_9874_{str(index)}"
+    text_line.attrib["custom"] = f"readingOrder {{index: {str(index)};}}"
+
+    coords_points = etree.SubElement(text_line, "Coords")
+    coords_points.attrib["points"] = text_line_coords
+    text_equiv = etree.SubElement(text_line, "TextEquiv")
+    unicode_field = etree.SubElement(text_equiv, "Unicode")
+    unicode_field.text = unicode_text
+
+    return text_line
+
+
+def build_xml_document(
+    image: np.array,
+    image_name: str,
+    text_region_bbox,
+    coordinates: list,
+    text_lines: list,
+):
+    root = etree.Element("PcGts")
+    root.attrib[
+        "xmlns"
+    ] = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15"
+    root.attrib["xmlns:xsi"] = "http://www.w3.org/2001/XMLSchema-instance"
+    root.attrib[
+        "xsi:schemaLocation"
+    ] = "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15 http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15/pagecontent.xsd"
+
+    metadata = etree.SubElement(root, "Metadata")
+    creator = etree.SubElement(metadata, "Creator")
+    creator.text = "Transkribus"
+    created = etree.SubElement(metadata, "Created")
+    created.text = get_utc_time()
+
+    page = etree.SubElement(root, "Page")
+    page.attrib["imageFilename"] = image_name
+    page.attrib["imageWidth"] = f"{image.shape[1]}"
+    page.attrib["imageHeight"] = f"{image.shape[0]}"
+
+    reading_order = etree.SubElement(page, "ReadingOrder")
+    ordered_group = etree.SubElement(reading_order, "OrderedGroup")
+    ordered_group.attrib["id"] = f"1234_{0}"
+    ordered_group.attrib["caption"] = "Regions reading order"
+
+    region_ref_indexed = etree.SubElement(reading_order, "RegionRefIndexed")
+    region_ref_indexed.attrib["index"] = "0"
+    region_ref = "region_main"
+    region_ref_indexed.attrib["regionRef"] = region_ref
+
+    text_region = etree.SubElement(page, "TextRegion")
+    text_region.attrib["id"] = region_ref
+    text_region.attrib["custom"] = "readingOrder {index:0;}"
+
+    text_region_coords = etree.SubElement(text_region, "Coords")
+    text_region_coords.attrib["points"] = text_region_bbox
+
+    # print(f"Adding Text to Document: {len(coordinates)}, {len(text_lines)}")
+    for i in range(0, len(coordinates)):
+        text_coords = get_text_points(coordinates[i])
+        # print(f"Unicode: {text_lines[i]}")
+        if text_lines != None:
+            text_region.append(
+                get_text_line_block(text_coords, i, unicode_text=text_lines[i])
+            )
+        else:
+            text_region.append(get_text_line_block(text_coords, i, unicode_text=""))
+
+    xmlparse = minidom.parseString(etree.tostring(root))
+    prettyxml = xmlparse.toprettyxml()
+
+    return prettyxml
